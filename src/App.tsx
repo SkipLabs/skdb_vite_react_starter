058458339819bb52cb4de96b39a1625a7aecd344
Greg Sexton <gregsexton@gmail.com>
2024-02-01 15:16:56 +0000

    Initial example app

---
+++ src/App.tsx
@@ -0,0 +1,56 @@
+import { useQuery, useSKDB } from 'skdb-react'
+import './App.css'
+
+function AddRandomRow() {
+  const db = useSKDB();
+  return (
+    <button
+      onClick={(_e) => {
+        const i = Math.round(Math.random() * 1000);
+        const f = Math.random();
+        db.exec(
+          "INSERT INTO example (intCol, floatCol, skdb_access) VALUES (@i, @f, 'read-write')",
+          {i, f}
+        );
+      }}>
+        Add Random Row
+    </button>
+  )
+}
+
+function ExampleTable() {
+  const rows = useQuery("SELECT id, intCol, floatCol FROM example");
+  const acc = rows.map(
+    row => (
+      <tr key={row.id}>
+        <td>{row.id}</td>
+        <td>{row.intCol}</td>
+        <td>{row.floatCol}</td>
+      </tr>
+    )
+  );
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>id</th>
+          <th>intCol</th>
+          <th>floatCol</th>
+        </tr>
+      </thead>
+      <tbody>{acc}</tbody>
+    </table>
+  )
+}
+
+function App() {
+  return (
+    <>
+      <AddRandomRow />
+      <hr />
+      <ExampleTable />
+    </>
+  )
+}
+
+export default App
